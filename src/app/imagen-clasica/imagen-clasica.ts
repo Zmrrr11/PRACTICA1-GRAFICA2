712e15a6a69bb92423ec4ac2ba7b1d79c56a4710
Zmrrr11 <zamirmarca90@gmail.com>
2026-02-18 21:04:29 -0400

    creacion actualizador y avanze visual

--- src/app/imagen-clasica/imagen-clasica.ts
+++ src/app/imagen-clasica/imagen-clasica.ts
@@ -11,5 +11,12 @@ export class ImagenClasicaComponent {
   brillo = computed(() => this.clasico() ? 120 : 100);
   contraste = computed(() => this.clasico() ? 150 : 100);
   gris = computed(() => this.clasico() ? 100 : 0);
+
+  actualizado = computed(() => {
+    return `brightness(${this.brillo()}%) contrast(${this.contraste()}%) grayscale(${this.gris()}%)`;
+  });
+  toggleEfecto(): void {
+    this.clasico.update(estado => !estado);
+  }
 }
 
